a572ae7b9447475121d8edda32be703745ec33ff
MAUI Messenger <maui-messenger@internet.ru>
2022-12-06 20:30:29 +0300

    Edit default pages, add claims to pages

--- Tasko.Client/ViewModels/LoginViewModel.cs
+++ Tasko.Client/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Json;
+using Tasko.Domains.Models.DTO.Providers;
 
 namespace Tasko.Client.ViewModels
 {
@@ -15,13 +16,34 @@ namespace Tasko.Client.ViewModels
 
         public bool LoginFailureHidden { get; set; } = true;
 
-        public async Task<bool> ValidateLoginAsync(out string jwtToken)
+        public async Task<string> ValidateLoginAsync()
         {
-            
 
-            jwtToken = null;
+            var user = new UserAuth
+            {
+                Login = Username,
+                Password = Password
+            };
+
+            JsonContent content = JsonContent.Create(user);
+
+            using (var client = new HttpClient())
+            {
+
+                var response = await client.PostAsync("http://87.249.49.56:7177/api/authorization", content);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jwtToken = await response.Content.ReadAsStringAsync();
+
+                    return jwtToken;
+                }
+
+
+            }
+
             LoginFailureHidden = false;
-            return false;
+            return string.Empty;
         }
     }
 }
---
+++ Tasko.Client/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,69 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Json;
+using Tasko.Domains.Models.DTO.Providers;
+
+namespace Tasko.Client.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Display(Name = "Логин")]
+        [Required(ErrorMessage = "Логин обязателен для заполнения")]
+        public string Username { get; set; }
+
+        [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Пароль обязателен для заполнения")]
+        public string Password { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email обязателен для заполнения")]
+        public string Email { get; set; }
+
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя обязательно для заполнения")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Фамилия обязательна для заполнения")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
+        [Required(ErrorMessage = "Отчество обязательно для заполнения")]
+        public string Patronymic { get; set; }
+
+        public bool LoginFailureHidden { get; set; } = true;
+
+        public async Task<string> RegistrationAsync()
+        {
+
+            var user = new UserCreate
+            {
+                Login = Username,
+                Password = Password,
+                Email = Email,
+                FirstName = FirstName,
+                LastName = LastName,
+                Patronymic = Patronymic
+            };
+
+            JsonContent content = JsonContent.Create(user);
+
+            using (var client = new HttpClient())
+            {
+
+                var response = await client.PostAsync("http://87.249.49.56:7177/api/users", content);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                {
+                    var jwtToken = await response.Content.ReadAsStringAsync();
+
+                    return jwtToken;
+                }
+
+
+            }
+
+            LoginFailureHidden = false;
+            return string.Empty;
+        }
+    }
+}
