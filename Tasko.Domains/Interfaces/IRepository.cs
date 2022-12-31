82e899b5c7ca7d0f8c7fd226f789d3667d00fda6
Bryzgalov Sergey <theseww4@gmail.com>
2022-12-31 09:36:55 +0300

    Add IRepository to Tasko.Domains

---
+++ Tasko.Domains/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Tasko.Domains.Interfaces
+{
+    public interface IRepository<Model, DtoCreateModel, DtoUpdateModel> 
+    where Model : class
+    where DtoCreateModel : class
+    where DtoUpdateModel : class
+    {
+        public Task<Model> FindAsync(Guid id);
+        public Task<Model> CreateAsync(DtoCreateModel dtoCreateModel);
+        public Task<Model> UpdateAsync(DtoUpdateModel dtoUpdateModel);
+        public Task DeleteAsync(Guid guid);
+        public Task<IEnumerable<Model>> GetAsync();
+    }
+}
