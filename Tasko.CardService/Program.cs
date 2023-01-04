b6fe18157adffae7551cb50cecf3a02c9959f596
brizgalov sergey <theseww4@gmail.com>
2023-01-04 11:17:51 +0300

    Small changes

--- Tasko.CardService/Program.cs
+++ Tasko.CardService/Program.cs
@@ -3,11 +3,10 @@ using Tasko.CardService.Infrastructure.Repositories;
 using Tasko.CardService.Infrastructure.Services;
 
 var builer = WebApplication.CreateBuilder(args);
+builer.Services.AddTransient<ICardRepository, CardRepository>();
 builer.Services.AddGrpc();
 builer.Services.AddGrpcReflection();
-builer.Services.AddTransient<ICardRepository, CardRepository>();
 var app = builer.Build();
 app.MapGrpcService<CardService>();
 app.MapGrpcReflectionService();
-app.MapGet("/",() => "Test");
 app.Run();
